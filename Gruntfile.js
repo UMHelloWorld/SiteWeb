a461f411251363c35aebb4b5b81614b337e02ea8
Lucas Franceschino <lucas@franceschino.fr>
2015-09-11 11:36:58 +0200

    Support compilation JS & CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,54 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-babel');
-
   grunt.initConfig({
-      babel: {
+      watch: {
+        scripts: {
+          files: ['**/*.es6.js'],
+          tasks: ['default'],
           options: {
-              sourceMap: true
-          },
-          dist: {
-              files: [{
-                expand: true,
-                src: '**/*.es6.js',
-                dest: './',
-                ext: '.js'
-              }]
+            spawn:false
           }
+        },
+        stylus: {
+          files: ['./static/styles/**/*.styl'],
+          tasks: ['compileCss'],
+          options: {
+            spawn:false
+          }
+        }
+      },
+      babel: {
+        options: {
+            sourceMap: true
+        },
+        dist: {
+            files: [{
+              expand: true,
+              src: '**/*.es6.js',
+              dest: './',
+              ext: '.js',
+            }]
+        }
+      },
+      stylus: {
+        compile: {
+          expand: true,
+          cwd: 'static/styles',
+          src: ['**/*.styl'],
+          dest: 'static/styles',
+          filter: 'isFile',
+          ext: '.css'
+        }
       }
   });
 
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
   grunt.registerTask('default', ['babel']);
+  grunt.registerTask('compileCss', ['stylus:compile']);
+
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
 };
